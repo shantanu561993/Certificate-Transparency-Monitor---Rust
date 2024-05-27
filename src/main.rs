1b1bed92e276ef1f2a6191b4e684e48738f0f38b
shantanu561993@gmail.com <shantanu561993@gmail.com>
2024-05-27 10:52:51 +0800

    Works with wildcards

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod logs_reader;
 #[tokio::main]
 async fn main() {
 
-    // logs_reader::root_reader(Some("https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/wildcards.txt".to_string())).await;
-    logs_reader::root_reader(None).await;
+    logs_reader::root_reader(Some("https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/wildcards.txt".to_string())).await;
+    // logs_reader::root_reader(None).await;
 
 }
