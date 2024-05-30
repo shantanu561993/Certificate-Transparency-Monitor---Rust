a16d9de86d81e741b89a39e631988306fbc501d0
shantanu561993@gmail.com <shantanu561993@gmail.com>
2024-05-30 16:31:02 +0800

    Add functionality to dump a single chain

--- src/logs_reader/mod.rs
+++ src/logs_reader/mod.rs
@@ -1,23 +1,104 @@
 mod logs_structs;
+use core::task;
 use std::collections::HashMap;
+use clap::builder::Str;
 use reqwest::Client;
 use logs_structs::{Root,Tree};
 use std::boxed::Box;
-
 use crate::generic_utils::{read_base64_entries, Entries};
 use crate::merkle_tree::utils::read_entry;
 use regex::RegexSet;
+use tokio::sync::Semaphore;
+use std::sync::Arc;
 
 
 
-
-
-pub async fn root_reader(bounty_target:Option<Vec<String>>){
+async fn get_all_operators()->HashMap<String, String>{
     let client:Client = Client::new();
     const ROOT_URL:&str =  "https://www.gstatic.com/ct/log_list/v3/all_logs_list.json";
     let data =  send_request(&ROOT_URL.to_string(), client.clone()).await.unwrap_or_else(|error| panic!("Unable to send request. {error}"));
     let parsed_root : Root = serde_json::from_str(&data).unwrap_or_else(|error| panic!("Unable to parse root {error}"));
     let operators = get_log_sources(parsed_root).await;
+    return operators;
+}
+pub async fn get_all_working_operators()->HashMap<String,String>{
+    let operators = get_all_operators().await;
+    let operators_in_list: Vec<(String,String)> = operators.iter().map(|(key, value)| (key.clone(),value.clone())).collect();
+    let client:Client = Client::new();
+    let mut tasks = Vec::new();
+    for (name, url) in &operators_in_list {
+        let mut info_endpoint = url.clone();
+        info_endpoint.push_str("ct/v1/get-sth");
+        tasks.push(send_request_with_string(info_endpoint.to_string().clone(), &client));
+    }
+    let responses = futures::future::join_all(tasks).await;
+    let mut return_hashmap:HashMap<String,String> = HashMap::new();
+    for (index,response) in responses.iter().enumerate(){
+        match response{
+            Ok(_)=>{
+                return_hashmap.insert(operators_in_list[index].0.clone(),operators_in_list[index].1.clone());
+            },
+            Err(_)=>continue,
+        }
+    }
+    return return_hashmap;
+}
+
+pub async fn dump_single_operator(url:&String){
+    let client:Client = Client::new();
+    let mut endpoint = url.clone();
+    endpoint.push_str("ct/v1/get-sth");
+    let response = send_request(&endpoint, client.clone()).await.unwrap();
+    let tree:Tree = serde_json::from_str(&response).unwrap();
+    let tree_size = tree.tree_size;
+    let mut size_request = url.clone();
+    size_request.push_str("ct/v1/get-entries?start=0&end=10000");
+    let response = send_request(&size_request, client.clone()).await.unwrap();
+    let batch_size = read_base64_entries(response).await.unwrap().entries.len();
+    println!("Tree Size: {tree_size},batch_size: {batch_size}");
+    let mut start = 0;
+    let mut tasks = Vec::new();
+    println!("Starting to dump the entries.. Wait for a while.");
+    let semaphore = Arc::new(Semaphore::new(100));
+
+    while start <= tree_size{
+
+        let mut watch_request = url.clone();
+        let end = start+(batch_size as i64);
+        watch_request.push_str(&format!("ct/v1/get-entries?start={start}&end={end}")[..]);
+        let permit = semaphore.clone().acquire_owned().await.unwrap();
+
+        let task = tokio::spawn(async {
+            let result = temp_function(watch_request).await;
+            // Drop the permit to release it
+            drop(permit);
+            result
+        });
+        tasks.push(task);
+        start = start + (batch_size as i64);
+    }
+    let responses = futures::future::join_all(tasks).await;
+}
+async fn temp_function (url:String) {
+    let client:Client = Client::new();
+    let mut resp: String = String::new() ;
+    loop{
+        match send_request_with_string(url.clone(), &client).await{
+            Ok(response)=>{resp.push_str(&response); break},
+            Err(_)=>{println!("{url}");continue}
+        }
+    }
+    let entries = read_base64_entries(resp).await.unwrap();
+    for entry in entries.entries{
+        let results = read_entry(&entry).await;
+        println!("{:?}",results);
+    }
+}
+
+
+pub async fn root_reader(bounty_target:Option<Vec<String>>){
+    let client:Client = Client::new();
+    let operators  = get_all_operators().await;
     let mut tasks = Vec::new();
     let mut regex_set:RegexSet = RegexSet::empty();
     if bounty_target.clone().is_some(){
@@ -49,6 +130,11 @@ pub async fn root_reader(bounty_target:Option<Vec<String>>){
 
 
 async fn send_request(url: &String, client: Client) -> Result<String, Box<dyn std::error::Error>> {
+    let response = client.get(url).send().await?.error_for_status()?;
+    let body = response.text().await?;
+    return Ok(body);   
+}
+async fn send_request_with_string(url: String, client: &Client) -> Result<String, Box<dyn std::error::Error>> {
     let response = client.get(url).send().await?.error_for_status()?;
     let body = response.text().await?;
     return Ok(body);   
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-#[allow(dead_code,unused_variables)]
+#[allow(dead_code,unused_variables,dropping_copy_types)]
 pub mod merkle_tree;
 pub mod generic_utils;
 mod logs_reader;
 use clap::{Arg, ArgGroup, Command};
-
 
 #[tokio::main]
 async fn main() {
     let matches = Command::new("Certificate Transparency Lists Monitor")
     .version("0.1")
     .author("Shantanu Khandelwal<shantanukhandelwal@protonmail.com>")
-    .about("Cyber Security Researcher")
+    .about("Tool to monitor and dump Certificate Transparency Lists")
     .arg(
         Arg::new("regex")
             .long("regex")
@@ -22,6 +21,12 @@ async fn main() {
             .long("url")
             .value_name("URL")
             .help("A URL to list of wildcards of bugbounty.")
+    )
+    .arg(
+        Arg::new("dump")
+            .long("dump")
+            .value_name("dump")
+            .help("Dump a single Certificate Transparency List.")
     )
     .group(
         ArgGroup::new("input")
@@ -35,6 +40,26 @@ async fn main() {
     }
     else if let Some(url) = matches.get_one::<String>("url") {
         logs_reader::root_reader(Some(vec!["URL".to_string(),url.to_string()])).await
+    }
+    else if let Some(dump) = matches.get_one::<String>("dump"){
+        println!("Please wait while we fetch the list of all the operators. This may take a while.");
+        let operators = logs_reader::get_all_working_operators().await;
+        let main_list: Vec<(String,String)> = operators.iter().map(|(key, value)| (key.clone(),value.clone())).collect();
+        drop(operators);
+        println!("Select the Log Source to parse:");
+        for (index, (name,url)) in main_list.iter().enumerate() {
+            println!("{index}: {name}=>{url}");
+        }
+        println!("Enter the index of the Log Source to parse:");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let index = input.trim().parse::<usize>().unwrap();
+        let (name,url) = main_list[index].clone();
+        drop(index);
+        drop(main_list);
+        drop(input);
+        let mut client : reqwest::Client = reqwest::Client::new();
+        logs_reader::dump_single_operator(&url).await;
     }
     else{
         logs_reader::root_reader(None).await;
